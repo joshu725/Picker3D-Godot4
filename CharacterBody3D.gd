51bfdf0d0f272c509932181236bcc7a027832b33
Joshua AR <joshu725@gmail.com>
2024-04-30 21:53:22 -0600

    v1.041 - Arreglo a bug que hacia mal focus a la ventana en Windows

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -1,22 +1,24 @@
 extends CharacterBody3D
 
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-# Se obtiene la gravedad predeterminada del proyecto
+# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+
 func _physics_process(delta):
-	# Añade la gravedad
+	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	
-	# Configura el salto
+
+	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		$Sonido_Salto.play()
 		velocity.y = JUMP_VELOCITY
-	
-	# Obtiene la dirección que se estáasdw   pulsando y configura el movimiento/desaceleración
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -25,8 +27,5 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
+
 	move_and_slide()
-	
-	# Configurar la camara para que se relacione con la posicion del personaje
-	$Control_Camara.position = lerp($Control_Camara.position, position, 0.15)
